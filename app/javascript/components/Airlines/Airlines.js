0c1e872a2ed64ce98b677b81127f3f0a9efb0a3b
github username <github email address>
2021-07-09 12:21:23 -0700

    airline.js

--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -1,7 +1,34 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState, useEffect } from 'react'
+import Airline from './Airline'
 
 const Airlines = () => {
-    return <div>This is the Airlines#index view for our app.</div>
+    const [airlines, setAirlines] = useState([])
+
+    useEffect(() => {
+
+        axios.get('/api/v1/airlines.json')
+        .then( resp => {
+            setAirlines(resp.data.data)
+        })
+        .catch( resp => console.log(resp) )
+    }, [airlines.length])
+
+    const list = airlines.map( item => {
+        return (<li key={item.attributes.name}>{item.attributes.name}</li>)
+    })
+
+    return (
+    <div className="home">
+        <div className="header">
+            <h1>OpenFlights</h1>
+            <div className="subheader"> Airline Reviews </div>
+        </div>
+    <div className="grid">
+    <ul>{list}</ul>
+    </div>
+</div>
+    )
 }
 
 export default Airlines
